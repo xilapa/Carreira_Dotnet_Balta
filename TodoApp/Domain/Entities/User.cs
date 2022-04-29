a7b3b409cba658d6a2dd2f1d53f5c775a5c7c46d
dirceu junior <dirceu.sjr@gmail.com>
2022-04-29 15:02:38 -0300

    TodoApp : Entidade user

---
+++ TodoApp/Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoApp.Domain.Entities;
+
+public class User : Entity
+{
+    public User(string googleId, string name)
+    {
+        GoogleId = googleId;
+        Name = name;
+        Active = true;
+    }
+
+    public string GoogleId { get; private set; }
+    public string Name { get; private set; }
+    public bool Active { get; private set; }
+
+    public void Deactivate() => Active = false;
+}
