b72eee31f5f897433e9b8df2c27d71e6646e9c16
dirceu junior <dirceu.sjr@gmail.com>
2022-04-29 14:49:37 -0300

    TodoApp : Entidade base criada

---
+++ TodoApp/Domain/Entities/Entity.cs
@@ -0,0 +1,26 @@
+﻿namespace TodoApp.Domain.Entities;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; private set; }
+
+    public bool Equals(Entity? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Id.Equals(other.Id);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Entity) obj);
+    }
+}
