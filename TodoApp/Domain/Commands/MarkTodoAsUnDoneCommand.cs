399fe766a3ad424bcffe05bf0f75f8473d46b2b5
dirceu junior <dirceu.sjr@gmail.com>
2022-04-29 16:51:38 -0300

    TodoApp : Comando para marcar todo como não feito

---
+++ TodoApp/Domain/Commands/MarkTodoAsUnDoneCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoApp.Domain.Commands;
+
+public class MarkTodoAsUnDoneCommand : Command<MarkTodoAsUnDoneCommand>
+{
+    public MarkTodoAsUnDoneCommand() : base(new MarkTodoAsUnDoneCommandValidator())
+    { }
+
+    public MarkTodoAsUnDoneCommand(Guid id, Guid userId) : this()
+    {
+        Id = id;
+        UserId = userId;
+    }
+
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+}
---
+++ TodoApp/Domain/Commands/MarkTodoAsUnDoneCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace TodoApp.Domain.Commands;
+
+public class MarkTodoAsUnDoneCommandValidator : AbstractValidator<MarkTodoAsUnDoneCommand>
+{
+    public MarkTodoAsUnDoneCommandValidator()
+    {
+        RuleFor(c => c.Id)
+            .NotEmpty().WithMessage("O id do todo não pode ser vazio");
+
+        RuleFor(c => c.UserId)
+            .NotEmpty().WithMessage("O id do usuário não pode ser vazio");
+    }
+}
