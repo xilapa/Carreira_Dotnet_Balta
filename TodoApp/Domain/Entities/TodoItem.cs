8088ce5dc86262459f7dfe672cc3b0bfa27980c5
dirceu junior <dirceu.sjr@gmail.com>
2022-04-29 14:57:58 -0300

    TodoApp : Entidade todo item

---
+++ TodoApp/Domain/Entities/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿namespace TodoApp.Domain.Entities;
+
+public class TodoItem : Entity
+{
+    public TodoItem(string title, DateTime date, Guid userId)
+    {
+        Title = title;
+        Done = false;
+        Date = date;
+        UserId = userId;
+    }
+
+    public string Title { get; private set; }
+    public bool Done { get; private set; }
+    public DateTime Date { get; private set; }
+    public Guid UserId { get; private set; }
+
+    public void MarkAsDone() => Done = true;
+
+    public void MarkAsUndone() => Done = false;
+
+    public void UpdateTitle(string title) => Title = title;
+}
