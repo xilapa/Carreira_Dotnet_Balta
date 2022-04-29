d929a022bb30f747d9044b23d012f30242bf30f4
dirceu junior <dirceu.sjr@gmail.com>
2022-04-29 16:46:19 -0300

    TodoApp : Comando para marcar todo como feito

---
+++ TodoApp/Domain/Commands/MarkTodoAsDoneCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoApp.Domain.Commands;
+
+public class MarkTodoAsDoneCommand : Command<MarkTodoAsDoneCommand>
+{
+    public MarkTodoAsDoneCommand() : base(new MarkTodoAsDoneCommandValidator())
+    { }
+
+    public MarkTodoAsDoneCommand(Guid id, Guid userId) : this()
+    {
+        Id = id;
+        UserId = userId;
+    }
+
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+}
---
+++ TodoApp/Domain/Commands/MarkTodoAsDoneCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace TodoApp.Domain.Commands;
+
+public class MarkTodoAsDoneCommandValidator : AbstractValidator<MarkTodoAsDoneCommand>
+{
+    public MarkTodoAsDoneCommandValidator()
+    {
+        RuleFor(c => c.Id)
+            .NotEmpty().WithMessage("O id do todo não pode ser vazio");
+
+        RuleFor(c => c.UserId)
+            .NotEmpty().WithMessage("O id do usuário não pode ser vazio");
+    }
+}
