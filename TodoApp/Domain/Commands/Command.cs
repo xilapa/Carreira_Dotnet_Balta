d6c7210187e7ac965a73832849d2e600e2e7c1fd
dirceu junior <dirceu.sjr@gmail.com>
2022-04-29 16:38:38 -0300

    TodoApp : Criação de comando base

---
+++ TodoApp/Domain/Commands/Command.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using TodoApp.Domain.Commands.Contracts;
+
+namespace TodoApp.Domain.Commands;
+
+public abstract class Command<T> : ICommand where T : class
+{
+    private readonly AbstractValidator<T> _validator;
+    private List<string> _errors { get; set; }
+
+    protected Command(AbstractValidator<T> validator)
+    {
+        if(validator is null)
+            throw new ArgumentNullException(nameof(validator));
+
+        _validator = validator;
+        _errors = new List<string>();
+    }
+    public IReadOnlyCollection<string> Errors => _errors.ToArray();
+
+    public bool Validate()
+    {
+        var validationResult = _validator.Validate((this as T)!);
+        _errors.AddRange(validationResult.Errors.Select(e => e.ErrorMessage));
+        return validationResult.IsValid;
+    }
+}
--- TodoApp/Domain/Commands/CreateTodoCommand.cs
+++ TodoApp/Domain/Commands/CreateTodoCommand.cs
@@ -3,32 +3,19 @@ using TodoApp.Domain.Commands.Contracts;
 
 namespace TodoApp.Domain.Commands;
 
-public class CreateTodoCommand : ICommand
+public class CreateTodoCommand : Command<CreateTodoCommand>
 {
-    private readonly AbstractValidator<CreateTodoCommand> _validator;
-    private List<string> _errors { get; set; }
+    public CreateTodoCommand() : base(new CreateTodoCommandValidator())
+    { }
 
-    public CreateTodoCommand() { }
-
-    public CreateTodoCommand(string title, Guid userId, DateTime date)
+    public CreateTodoCommand(string title, Guid userId, DateTime date) : this()
     {
         Title = title;
         UserId = userId;
         Date = date;
-        _errors = new List<string>();
-        _validator = new CreateTodoCommandValidator();
     }
 
     public string Title { get; set; }
     public Guid UserId { get; set; }
     public DateTime Date { get; set; }
-
-    public IReadOnlyCollection<string> Errors => _errors.ToArray();
-
-    public bool Validate()
-    {
-        var validationResult = _validator.Validate(this);
-        _errors.AddRange(validationResult.Errors.Select(e => e.ErrorMessage));
-        return validationResult.IsValid;
-    }
 }
