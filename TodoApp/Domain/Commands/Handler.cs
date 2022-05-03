5f940be5043c843d60734b033990a66ddd20165d
dirceu junior <dirceu.sjr@gmail.com>
2022-05-03 19:33:31 -0300

    TodoApp : create todo handler

---
+++ TodoApp/Domain/Commands/Handler.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Repositories;
+using TodoApp.Domain.Commands.Base;
+using TodoApp.Domain.Commands.Contracts;
+using TodoApp.Domain.Commands.CreateTodo;
+using TodoApp.Domain.Entities;
+
+namespace Domain.Commands;
+
+public class Handler : BaseHandler, IHandler<CreateTodoCommand>
+{
+    private readonly ITodoRepository _todoRepository;
+    private readonly IUnitOfWork _uow;
+
+    public Handler(ITodoRepository todoRepository, IUnitOfWork uow)
+    {
+        _todoRepository = todoRepository;
+        _uow = uow;
+    }
+
+    public async Task<ICommandResult> Handle(CreateTodoCommand command)
+    {
+        // Validar command
+        if(command.IsInvalid())
+            return new CommandResult(false, "A tarefa está errada", command.Errors);
+
+        // Criar todo
+        var todo = new TodoItem(command.Title, command.Date, command.UserId);
+
+        // Salvar todo no banco
+        _todoRepository.Create(todo);
+        await _uow.SaveChangesAsync();
+
+        // Retornar resultado
+        return new CommandResult(true, "Tarefa criada com sucesso", todo);
+    }
+}
