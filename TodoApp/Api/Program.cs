e9e0e9ff7d182f8c2efa7b16d4501cf01e79142b
dirceu junior <dirceu.sjr@gmail.com>
2022-05-04 20:21:48 -0300

    TodoApp : injeção de dependência

--- TodoApp/Api/Program.cs
+++ TodoApp/Api/Program.cs
@@ -1,3 +1,14 @@
+using Domain.Commands;
+using Domain.Repositories;
+using Infra.Contexts;
+using Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Domain.Commands.Contracts;
+using TodoApp.Domain.Commands.CreateTodo;
+using TodoApp.Domain.Commands.MarkTodoAsDone;
+using TodoApp.Domain.Commands.MarkTodoAsUndone;
+using TodoApp.Domain.Commands.UpdateTodo;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +17,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Infra
+builder.Services.AddDbContext<TodoDataContext>(opt => opt.UseInMemoryDatabase("testeDb"));
+builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+
+// Domain
+builder.Services.AddScoped<TodoHandler>();
+builder.Services.AddScoped<IHandler<CreateTodoCommand>>(s => s.GetRequiredService<TodoHandler>());
+builder.Services.AddScoped<IHandler<UpdateTodoCommand>>(s => s.GetRequiredService<TodoHandler>());
+builder.Services.AddScoped<IHandler<MarkTodoAsDoneCommand>>(s => s.GetRequiredService<TodoHandler>());
+builder.Services.AddScoped<IHandler<MarkTodoAsUndoneCommand>>(s => s.GetRequiredService<TodoHandler>());
 
 var app = builder.Build();
 
