b9eef3ed1314b6310993ce1f00bbcfdbe0ea51a4
dirceu junior <dirceu.sjr@gmail.com>
2022-05-04 20:03:00 -0300

    TodoApp : criação do contexto de dados

---
+++ TodoApp/Infra/Contexts/TodoDataContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Domain.Entities;
+
+namespace Infra.Contexts;
+
+public class TodoDataContext : DbContext
+{
+    public TodoDataContext(DbContextOptions<TodoDataContext> options) : base(options)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TodoItem>()
+            .HasKey(t => t.Id);
+
+        modelBuilder.Entity<TodoItem>()
+            .Property(t => t.Id).HasColumnType("uniqueidentifier");
+
+        modelBuilder.Entity<TodoItem>()
+            .Property(t => t.Title)
+            .HasMaxLength(200)
+            .HasColumnType("varchar(200)")
+            .IsRequired();
+
+        modelBuilder.Entity<TodoItem>()
+            .Property(t => t.Done)
+            .HasColumnType("bit")
+            .IsRequired();
+
+        modelBuilder.Entity<TodoItem>()
+            .HasOne<User>()
+            .WithMany()
+            .HasForeignKey(t => t.UserId);
+
+        modelBuilder.Entity<TodoItem>()
+            .HasIndex(t => t.UserId);
+
+        modelBuilder.Entity<User>()
+            .HasKey(u => u.Id);
+
+        modelBuilder.Entity<User>()
+            .Property(u => u.GoogleId)
+            .HasMaxLength(100)
+            .HasColumnType("varchar(100)")
+            .IsRequired();
+
+        modelBuilder.Entity<User>()
+            .Property(u => u.Name)
+            .HasMaxLength(200)
+            .HasColumnType("varchar(200)")
+            .IsRequired();
+
+        modelBuilder.Entity<User>()
+            .Property(u => u.Active)
+            .HasColumnType("bit")
+            .IsRequired();
+    }
+
+    public DbSet<TodoItem> Todos { get; set; }
+}
